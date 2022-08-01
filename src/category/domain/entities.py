fd04ded4110344e49b774e7372ebd4f476542488
Lucas Montenegro <lucas.montenegro@bavabank.com>
2022-08-01 09:17:50 -0300

    fix: add uuid to entity category

--- src/category/domain/entities.py
+++ src/category/domain/entities.py
@@ -1,12 +1,19 @@
+# from src.category.domain.entities import Category
 from datetime import datetime
 from dataclasses import dataclass, field
+import uuid
 
-from pyparsing import Optional
+from typing import Optional
 
-@dataclass
+# KW_ONLY=True -> obriga utilizacao de parametros nomeados
+
+
+@dataclass(kw_only=True)
 class Category:
 
+    id: uuid.UUID = field(default_factory=lambda: uuid.uuid4())
     name: str
     description: Optional[str] = None
     is_active: Optional[bool] = True
-    created_at: Optional[datetime] = field(default_factory=lambda: datetime.now())
+    created_at: Optional[datetime] = field(
+        default_factory=lambda: datetime.now())
