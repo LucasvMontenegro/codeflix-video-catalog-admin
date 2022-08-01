4f522469ce82fd4cfa97b814782da4a9caf4e47c
Lucas Montenegro <lucas.montenegro@bavabank.com>
2022-08-01 09:37:27 -0300

    test: vo entity id

---
+++ src/__seedwork/tests/unit/domain/test_unit_value_objects.py
@@ -0,0 +1,10 @@
+from dataclasses import is_dataclass
+import unittest
+
+from src.__seedwork.domain.value_objects import UniqueEntityId
+
+
+class TestUniqueEntityIdUnit(unittest.TestCase):
+
+    def test_if_is_a_dataclass(self):
+        self.assertTrue(is_dataclass(UniqueEntityId))
